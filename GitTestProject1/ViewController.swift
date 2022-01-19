c98a8b4935e7076da5dc804616f1deefe0b9df75
tora <masaya.development@gmail.com>
2022-01-19 21:14:11 +0900

    Initial Commit

---
+++ GitTestProject1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProject1
+//
+//  Created by Masaya Fujioka on 2022/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("アイウエオ")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
