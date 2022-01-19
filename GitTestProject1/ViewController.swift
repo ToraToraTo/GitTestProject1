36a6525dc35b8e75dbc3b948154c29315294cb7a
tora <masaya.development@gmail.com>
2022-01-19 21:18:03 +0900

    print(かきくけこ)を追記しました。　トラ

--- GitTestProject1/ViewController.swift
+++ GitTestProject1/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("アイウエオ")
+        
+        print("かきくけこ")
         // Do any additional setup after loading the view.
     }
 
